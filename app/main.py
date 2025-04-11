e8f154606efe3c198091a3075d44a6b3f262a41b
Oliver Gregorius <oliver@gregorius.dev>
2025-04-11 15:38:36 +0200

    Included health endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.routes import mutations
+from app.routes import mutations, monitoring
 
 app = FastAPI()
 app.include_router(mutations.router)
+app.include_router(monitoring.router)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
