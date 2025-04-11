ad83431ef01e00b6effeae85591ccb5be1647f66
Oliver Gregorius <oliver@gregorius.dev>
2025-04-11 23:37:46 +0200

    Enabling SSL

--- app/main.py
+++ app/main.py
@@ -7,11 +7,8 @@ from app.routes import mutations, monitoring
 
 app = FastAPI()
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain('/tls/tls.crt', keyfile='/tls/tls.key')
-
 app.include_router(mutations.router)
 app.include_router(monitoring.router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8443, log_level="info", ssl=ssl_context)
+    uvicorn.run(app, host='0.0.0.0', port=8000, log_level="debug")
